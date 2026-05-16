39613a279de810724d8a8ffa8b97ef42155a6c6f
DARREN <59622013+DARREN-2000@users.noreply.github.com>
2026-05-16 18:14:05 +0000

    chore: add ESLint, CI quality workflow, and lint scripts

---
+++ .eslintrc.cjs
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true },
+  },
+  settings: {
+    react: { version: 'detect' },
+  },
+  env: {
+    browser: true,
+    node: true,
+    es2022: true,
+  },
+  plugins: ['@typescript-eslint', 'react', 'react-hooks', 'jsx-a11y'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'prettier',
+  ],
+  rules: {
+    'react/prop-types': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-explicit-any': 'warn',
+    'jsx-a11y/no-noninteractive-element-interactions': 'off',
+  },
+};
